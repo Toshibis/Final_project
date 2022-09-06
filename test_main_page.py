f1dbf96b94026ce0743eda386083739525a480b4
toshibis <toshibiss@gmail.com>
2022-09-06 13:16:55 +0300

    Final project Adding Page transitions

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,13 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-import time
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_currect_login_url()
 
 
 def test_guest_should_see_login_link(browser):
@@ -23,11 +23,13 @@ def test_current_url(browser):
     page.open()
     page.should_be_currect_login_url()
 
+
 def test_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/es/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
+
 
 def test_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/es/accounts/login/"
